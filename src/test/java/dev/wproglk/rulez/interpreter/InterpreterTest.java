15a9a01a98b54271778e7738bb989a9dcd269ded
Lukas Keller <lukas.adrian.keller@bluewin.ch>
2026-02-21 16:38:48 +0100

    F - more tests for precedence of rules

--- src/test/java/dev/wproglk/rulez/interpreter/InterpreterTest.java
+++ src/test/java/dev/wproglk/rulez/interpreter/InterpreterTest.java
@@ -3,12 +3,52 @@ package dev.wproglk.rulez.interpreter;
 import dev.wproglk.rulez.rules.Rule;
 import dev.wproglk.rulez.rules.Ruleset;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Named.named;
 
 public class InterpreterTest {
+
+    public static Stream<Arguments> precedenceBaseExtraction() {
+
+        final String firstname = """
+                {
+                    "firstname": "Foo",
+                    "lastname": "Bar"
+                }
+                """;
+
+        final String lastname = """
+                {
+                    "lastname": "Bar"
+                }
+                """;
+
+        final String empty = """
+                {
+                
+                }
+                """;
+
+        final String otherAttribute = """
+                {
+                  "fullname": "Foo Bar"
+                }
+                """;
+
+        return Stream.of(
+                Arguments.of(named("entire json", firstname), "Foo"),
+                Arguments.of(named("single attribute json", lastname), "Bar"),
+                Arguments.of(named("irrelevant attribute json", otherAttribute), null),
+                Arguments.of(named("empty json", empty), null)
+        );
+    }
 
     @Test
     void extractFirstname() {
@@ -44,15 +84,10 @@ public class InterpreterTest {
         assertThat(result).isEqualTo("Bar");
     }
 
-    @Test
-    void precedenceBaseExtraction() {
+    @ParameterizedTest(name = "{0}")
+    @MethodSource
+    void precedenceBaseExtraction(String json, String expectedResult) {
         // arrange
-        final String json = """
-                {
-                    "lastname": "Bar"
-                }
-                """;
-
         final Rule firstname = new Rule("$.firstname");
         final Rule lastname = new Rule("$.lastname");
 
@@ -64,6 +99,6 @@ public class InterpreterTest {
         String result = Interpreter.execute(nameRuleset, json);
 
         // assert
-        assertThat(result).isEqualTo("Bar");
+        assertThat(result).isEqualTo(expectedResult);
     }
 }
